1262b04673572e63e9a946a829f9b7fc64aa544c
Calon-raja-ngoding <nbilhnief@gmail.com>
2024-10-31 07:15:39 +0800

    Modifikasi percobaan 4

--- SearchNilai18.java
+++ SearchNilai18.java
@@ -1,17 +1,31 @@
+import java.util.Scanner;
 public class SearchNilai18 {
     public static void main(String[] args) {
-        int [] arrNilai={80,85,78,96,90,82,86};
-        int key = 90;
-        int hasil =0;
-
+        Scanner sc = new Scanner(System.in);
+        int hasil =0,key,jmlhNilai;
+        System.out.print("Masukkan banyaknya nilai yg akan diinput : ");
+        jmlhNilai = sc.nextInt();
+        int [] arrNilai=new int[jmlhNilai];
+        for(int i =0 ; i<jmlhNilai;i++){
+            System.out.print("Masukkan nilai mahasiswa ke-" +(i+1)+ " : ");
+            arrNilai[i] = sc.nextInt();
+        }
+        System.out.print("Masukkan nilai yg ingin dicari : ");
+        key = sc.nextInt();
         for(int i =0; i < arrNilai.length;i++){
             if (key == arrNilai[i]) {
                 hasil = i;
+                System.out.println();
+                System.out.println("Nilai "+ key + " ketemu, merupakan nilai mahasiswa ke-"+ hasil);
+                System.out.println();
                 break;
             }
         }
-        System.out.println();
-        System.out.println("Nilai "+ key + " ketemu di index ke-"+ hasil);
-        System.out.println();
+        if (hasil == 0) {
+            System.out.println();
+            System.out.println("Nilai yg dicari tidak ditemukan ");
+            System.out.println();
+        }
+        
     }
 }
